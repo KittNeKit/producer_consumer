12e602fd3cf5b579b23e2cba1c329df8f18940ae
Nikita Provorov <nikiyorkovich@gmail.com>
2023-09-08 22:41:50 +0300

    secret key

--- producer_consumer/settings.py
+++ producer_consumer/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_df&2h4h%3l$fkv3l18#sp^gy&(hadxj*fat))drs546eje(bp"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
