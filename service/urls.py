9bc757335f0fe1c7896591a20a7fecf2d6dc92a8
Nikita Provorov <nikiyorkovich@gmail.com>
2023-09-08 18:13:21 +0300

    add fixture

--- service/urls.py
+++ service/urls.py
@@ -5,7 +5,7 @@ from service.views import OrderListView, delete_order
 urlpatterns = [
     path("", OrderListView.as_view(), name="order_list"),
     path(
-        "/<int:pk>/delete_order/",
+        "<int:pk>/delete_order/",
         delete_order,
         name="delete_order",
     ),
