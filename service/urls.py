a0e2ea38ebce261b816004459c416bd6d5e3122f
Nikita Provorov <nikiyorkovich@gmail.com>
2023-09-08 22:42:08 +0300

    class based view

--- service/urls.py
+++ service/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from service.views import OrderListView, delete_order
+from service.views import OrderListView, DeleteOrderView
 
 urlpatterns = [
     path("", OrderListView.as_view(), name="order_list"),
     path(
         "<int:pk>/delete_order/",
-        delete_order,
+        DeleteOrderView.as_view(),
         name="delete_order",
     ),
 ]
--- service/views.py
+++ service/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views import generic, View
 
 from service.models import Order
 from service.notification import send_delete_order_text
@@ -15,8 +15,9 @@ class OrderListView(LoginRequiredMixin, generic.ListView):
         return self.queryset.filter(employee=self.request.user)
 
 
-def delete_order(request, pk):
-    order = Order.objects.get(id=pk)
-    send_delete_order_text(order, request.user)
-    order.delete()
-    return HttpResponseRedirect(reverse_lazy("service:order_list"))
+class DeleteOrderView(View):
+    def post(self, request, pk):
+        order = Order.objects.get(id=pk)
+        send_delete_order_text(order, request.user)
+        order.delete()
+        return HttpResponseRedirect(reverse_lazy("service:order_list"))
