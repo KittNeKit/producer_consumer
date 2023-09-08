622388cd177c50deb61ce5467ad2f2d5bd5b12d3
Nikita Provorov <nikiyorkovich@gmail.com>
2023-09-08 22:41:57 +0300

    optimize

--- service/tasks.py
+++ service/tasks.py
@@ -1,17 +1,24 @@
 import random
 
 from celery import shared_task
+from django.db.models import Max
 
 from service.models import Order, Employee
 
 
 @shared_task
 def create_order():
-    employees = Employee.objects.all()
-    random_employee = random.choice(employees)
+    max_id = Employee.objects.all().aggregate(max_id=Max("id"))["max_id"]
+    while True:
+        pk = random.randint(1, max_id)
+        employee = Employee.objects.filter(pk=pk).first()
+        if employee:
+            break
+
+    task_id = random.randint(1, 100)
     Order.objects.create(
         name="Order from celery",
-        description="Test order",
-        employee=random_employee,
-        task_id=random.randint(1, 100),
+        description=f"Test order#{task_id}",
+        employee=employee,
+        task_id=task_id,
     )
